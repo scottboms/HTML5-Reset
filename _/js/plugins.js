61bd89abbbe6f908d41913b8eebb259bc645e916
Scott Boms <scottboms@gmail.com>
2011-08-09 17:21:08 +0000

    Adding a new jQuery plugins stub file.

---
+++ _/js/plugins.js
@@ -0,0 +1,6 @@
+/* 
+ * Required jQuery plugins
+ * Include copies of any required jQuery plugins into this file
+ * and use YUI or another JS minimizer to create a minimized and 
+ * optimized version of the file.
+ */
